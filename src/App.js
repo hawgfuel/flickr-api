a8023f3ed50122cc31ebf964b13c588e0cb073a0
Alan Werstler <hawgfuel@gmail.com>
2024-05-02 10:41:16 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Template } from  "./pages/template/template";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Template />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/template/template.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export function Template() {
+   
+    return (
+        <div className='container row bs-component col-lg-4 mx-auto d-grid gap-3'>
+          <h1 className='page-header p-2'>Template</h1>
+      </div>
+    )
+    
+}
