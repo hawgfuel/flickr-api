5f05985c499833cb53ebac70b1159616531837a7
Alan Werstler <hawgfuel@gmail.com>
2024-05-30 12:18:48 -0700

    updating pagination to show 2 siblings, first page and last page, conditional disable prev and next buttons

--- src/components/paginate/paginate.jsx
+++ src/components/paginate/paginate.jsx
@@ -10,21 +10,41 @@ export const Paginate = ({ postsPerPage, totalPosts, paginate, previousPage, nex
    return (
       <nav>
          <ul className="pagination">
-         <li onClick={previousPage} className="page-number">
-               Prev
-            </li>
-            {pageNumbers.map((number) => (
-               <li
-                  key={number}
-                  onClick={() => paginate(number)}
-                  className={"page-number page-item " + (currentPage === number ? 'active' : '')}
-               >
-                  {number}
-               </li>
-            ))}
-            <li onClick={nextPage} className="page-number">
-               Next
-            </li>
+            <li onClick={previousPage} className={"page-number + " + (currentPage === 1 ? ' disable' : '')}>Prev</li>
+            {currentPage > 3 && (
+                  <>
+                  <li
+                     key={pageNumbers[0]} onClick={() => paginate(pageNumbers[0])}
+                     className={"page-number page-item" + (currentPage === pageNumbers[0] ? ' active' : '')}>
+                     {pageNumbers[0]}
+                  </li>
+                  <li className='page-number page-item ellipsis'>...</li>
+                  </>
+               )}
+            {pageNumbers
+               .filter((number) => 
+                  number === currentPage - 1 || number === currentPage || number === currentPage + 1
+               )
+               .map((number) => (
+                  <li
+                     key={number}
+                     onClick={() => paginate(number)}
+                     className={"page-number page-item" + (currentPage === number ? ' active' : '')}
+                  >
+                     {number}
+                  </li>
+               ))}
+               {currentPage !== pageNumbers.length && currentPage + 1 !== pageNumbers.length && (
+                  <>
+                  <li className='page-number page-item ellipsis'>...</li>
+                  <li
+                     key={pageNumbers.length} onClick={() => paginate(pageNumbers.length)}
+                     className={"page-number page-item" + (currentPage === pageNumbers.length ? ' active' : '')}>
+                     {pageNumbers.length}
+                  </li>
+                  </>
+               )}
+               <li onClick={nextPage} className={"page-number + " + (currentPage === pageNumbers.length? ' disable' : '')}>Next</li>
          </ul>
       </nav>
    );
